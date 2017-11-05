30153e206891ac8573de2778b59c68b5a63535f8
Chris Margonis <chris.margonis@gmail.com>
2017-11-05 19:40:51 +0200

    Add list adapter Add mock values to populate the list

---
+++ app/src/main/java/me/cmargonis/ultimuskotlin/presentation/components/adapters/ForecastListAdapter.kt
@@ -0,0 +1,24 @@
+package me.cmargonis.ultimuskotlin.presentation.components.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by Chris Margonis on 11/5/2017.
+
+ */
+class ForecastListAdapter(private val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            ViewHolder(TextView(parent.context))
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
